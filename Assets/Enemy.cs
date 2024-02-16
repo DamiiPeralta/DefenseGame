44aa9250be346b2785861505907d702dba39f21f
DamianEPeralta <damian@dascalendar.com>
2024-02-16 19:43:05 -0300

    Primeras mecanicas terminadas

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -19,186 +19,186 @@ public class Enemy : MonoBehaviour
     public float attackSpeed;
     public int level;
     public Animator animator;
-    public GameObject merc; // Cambiado de MercenaryController a GameObject
+    public GameObject merc;
 
     public GameObject popUpDamagePrefab;
 
-    public float velocity = 3f;
+public float velocity = 3f;
 
-    public int enemyDamage = 1;
+public int enemyDamage = 1;
 
-    public int killReward = 1;
+public int killReward = 1;
 
-    public bool enemyInFront;
+public bool enemyInFront;
 
-    public GameObject enemyInFrontObj;
+public GameObject enemyInFrontObj;
 
-    void Start()
+void Start()
+{
+    actualHealth = health;
+    enemyInFront = false;
+}
+
+void Update()
+{
+    if (enemyInFrontObj == null)
     {
-        actualHealth = health;
         enemyInFront = false;
     }
-
-    void Update()
+    if (enemyInFrontObj != null)
     {
-        if (enemyInFrontObj == null)
-        {
-            enemyInFront = false;
-        }
-        if (enemyInFrontObj != null)
-        {
-            if (!enemyInFrontObj.GetComponent<Enemy>().alive)
-            {
-                enemyInFront = false;
-                enemyInFrontObj = null;
-            }
-        }
-        if (merc != null)
-        {
-            if (!merc.GetComponent<MercenaryController>().stats.alive)
-            {
-                merc = null;
-            }
-        }
-        if (alive && merc == null && !enemyInFront)
-        {
-            walk = true;
-        }
-        else
-        {
-            walk = false;
-        }
-        BattleLogic();
-        if (walk)
-        {
-            transform.Translate(Vector2.left * velocity * Time.deltaTime);
-        }
-        if (enemyInFrontObj == null)
-        {
-            enemyInFront = false;
-        }
-
-    }
-
-    public void Attack(int minAtk, int maxAtk)
-    {
-        System.Random randAtk = new System.Random();
-
-        int atk = randAtk.Next(minAtk, maxAtk + 1);
-        int totalDamage = atk;
-        Debug.Log(" the attack is " + atk + "total damage =" + totalDamage + " left life = " + merc.GetComponent<Stats>().health);
-
-
-        if (merc.GetComponent<MercenaryController>() != null)
-        {
-            merc.GetComponent<MercenaryController>().TakeDamage(atk);
-        }
-
-        animator.SetBool("Attack", true);
-    }
-
-    public void TakeDamage(int atk)
-    {
-        int attackDamageText;
-        actualHealth -= Mathf.Max(0, atk - defense);
-        attackDamageText = atk - defense;
-
-        ShowDamageText(attackDamageText);
-
-        if (actualHealth <= 0)
-        {
-            GameManager.Instance.money += killReward;
-            Death();
-        }
-    }
-
-    public void ShowDamageText(int damageAmount)
-    {
-        GameObject damageTextObject = Instantiate(popUpDamagePrefab.GetComponent<PopUpText>().damageTextPrefab, transform.position, Quaternion.identity);
-        damageTextObject.transform.parent = transform;
-        damageTextObject.transform.localPosition = new Vector3(0f, 2f, 0f);
-
-        TextMeshPro damageText = damageTextObject.GetComponent<TextMeshPro>();
-        damageText.text = damageAmount.ToString();
-        Destroy(damageTextObject, 4f);
-    }
-
-    public void Death()
-    {
-        alive = false;
-        coll.enabled = false;
-        animator.SetTrigger("Death");
-        Destroy(gameObject, 2f);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Merc")
-        {
-            merc = other.gameObject;
-        }
-        else if (other.tag == "EnemyBack")
-        {
-            enemyInFront = true;
-            Transform parentTransform = other.gameObject.transform.parent;
-            if (parentTransform != null)
-            {
-                GameObject parentObject = parentTransform.gameObject;
-                enemyInFrontObj = parentObject;
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "EnemyBack")
+        if (!enemyInFrontObj.GetComponent<Enemy>().alive)
         {
             enemyInFront = false;
             enemyInFrontObj = null;
         }
     }
-
-    public void BattleLogic()
+    if (merc != null)
     {
-        if (merc != null)
+        if (!merc.GetComponent<MercenaryController>().stats.alive)
         {
-            walk = false;
-            if (merc.GetComponent<MercenaryController>() != null)
+            merc = null;
+        }
+    }
+    if (alive && merc == null && !enemyInFront)
+    {
+        walk = true;
+    }
+    else
+    {
+        walk = false;
+    }
+    BattleLogic();
+    if (walk)
+    {
+        transform.Translate(Vector2.left * velocity * Time.deltaTime);
+    }
+    if (enemyInFrontObj == null)
+    {
+        enemyInFront = false;
+    }
+
+}
+
+public void Attack(int minAtk, int maxAtk)
+{
+    System.Random randAtk = new System.Random();
+
+    int atk = randAtk.Next(minAtk, maxAtk + 1);
+    int totalDamage = atk;
+    Debug.Log(" the attack is " + atk + "total damage =" + totalDamage + " left life = " + merc.GetComponent<Stats>().health);
+
+
+    if (merc.GetComponent<MercenaryController>() != null)
+    {
+        merc.GetComponent<MercenaryController>().TakeDamage(atk);
+    }
+
+    animator.SetBool("Attack", true);
+}
+
+public void TakeDamage(int atk)
+{
+    int attackDamageText;
+    actualHealth -= Mathf.Max(0, atk - defense);
+    attackDamageText = atk - defense;
+
+    ShowDamageText(attackDamageText);
+
+    if (actualHealth <= 0)
+    {
+        GameManager.Instance.money += killReward;
+        Death();
+    }
+}
+
+public void ShowDamageText(int damageAmount)
+{
+    GameObject damageTextObject = Instantiate(popUpDamagePrefab.GetComponent<PopUpText>().damageTextPrefab, transform.position, Quaternion.identity);
+    damageTextObject.transform.parent = transform;
+    damageTextObject.transform.localPosition = new Vector3(0f, 2f, 0f);
+
+    TextMeshPro damageText = damageTextObject.GetComponent<TextMeshPro>();
+    damageText.text = damageAmount.ToString();
+    Destroy(damageTextObject, 4f);
+}
+
+public void Death()
+{
+    alive = false;
+    coll.enabled = false;
+    animator.SetTrigger("Death");
+    Destroy(gameObject, 2f);
+}
+
+private void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.tag == "Merc")
+    {
+        merc = other.gameObject;
+    }
+    else if (other.tag == "EnemyBack")
+    {
+        enemyInFront = true;
+        Transform parentTransform = other.gameObject.transform.parent;
+        if (parentTransform != null)
+        {
+            GameObject parentObject = parentTransform.gameObject;
+            enemyInFrontObj = parentObject;
+        }
+    }
+}
+
+private void OnTriggerExit2D(Collider2D other)
+{
+    if (other.tag == "EnemyBack")
+    {
+        enemyInFront = false;
+        enemyInFrontObj = null;
+    }
+}
+
+public void BattleLogic()
+{
+    if (merc != null)
+    {
+        walk = false;
+        if (merc.GetComponent<MercenaryController>() != null)
+        {
+            if (merc.GetComponent<MercenaryController>().stats.alive)
             {
-                if (merc.GetComponent<MercenaryController>().stats.alive)
+                if (!cooling)
                 {
-                    if (!cooling)
-                    {
-                        Attack(minAttack, maxAttack);
-                        cooling = true;
-                    }
-                    else
-                    {
-                        Cooldown();
-                    }
+                    Attack(minAttack, maxAttack);
+                    cooling = true;
+                }
+                else
+                {
+                    Cooldown();
                 }
             }
         }
-        else
-        {
-            BackToIdle();
-            timer = attackSpeed;
-        }
     }
-
-    public void BackToIdle()
+    else
     {
-        animator.SetBool("Attack", false);
+        BackToIdle();
+        timer = attackSpeed;
     }
+}
 
-    void Cooldown()
+public void BackToIdle()
+{
+    animator.SetBool("Attack", false);
+}
+
+void Cooldown()
+{
+    timer -= Time.deltaTime;
+
+    if (timer <= 0 && cooling)
     {
-        timer -= Time.deltaTime;
-
-        if (timer <= 0 && cooling)
-        {
-            cooling = false;
-            timer = attackSpeed;
-        }
+        cooling = false;
+        timer = attackSpeed;
     }
+}
 }
--- Assets/MercenaryController.cs
+++ Assets/MercenaryController.cs
@@ -9,120 +9,120 @@ public class MercenaryController : MonoBehaviour
     // Tipo de mercenario
     public MercenaryStats mercenaryStats;
 
-    public Stats stats;
-    public Enemy enemy;
-    public Animator animator;
+public Stats stats;
+public Enemy enemy;
+public Animator animator;
 
-    public BoxCollider2D coll;
+public BoxCollider2D coll;
 
-    public bool cooling;
-    public float timer;
-    public int actualHealth;
+public bool cooling;
+public float timer;
+public int actualHealth;
 
-    private void Start() 
+private void Start() 
+{
+    
+    actualHealth = stats.health;
+    stats.stats = mercenaryStats;
+    stats.SetStatus();
+    stats.alive = true;
+    actualHealth = stats.health;  
+}
+
+private void Update() 
+{
+    BattleLogic();
+}
+
+public void Attack(int minAtk, int maxAtk)
+{
+    System.Random randAtk = new System.Random();
+    int atk = randAtk.Next(minAtk, maxAtk + 1);
+    int totalDamage = atk - enemy.defense;
+    Debug.Log(" the attack is " + atk + "total damage =" + totalDamage + " left life = " + enemy.actualHealth);
+
+    enemy.TakeDamage(atk);
+    animator.SetBool("Attack", true);
+    if(!enemy.alive)
     {
-        
-        actualHealth = stats.health;
-        stats.stats = mercenaryStats;
-        stats.SetStatus();
-        stats.alive = true;
-        actualHealth = stats.health;  
+        enemy = null;
     }
-
-    private void Update() 
+}
+public void TakeDamage(int atk)
+{
+    actualHealth -= Mathf.Max(0, atk - stats.defense);
+    
+    if(actualHealth <= 0)
     {
-        BattleLogic();
+        Death();   
     }
+    
+}
+public void Death()
+{
+    stats.alive = false;
+    coll.enabled = false;
+    animator.SetTrigger("Death");
+}
+public void LevelUp()
+{
 
-    public void Attack(int minAtk, int maxAtk)
+}
+public void TakeExp(int exp)
+{
+
+}
+public void Cooldown()
+{
+    timer -= Time.deltaTime;
+
+    if (timer <= 0 && cooling)
     {
-        System.Random randAtk = new System.Random();
-        int atk = randAtk.Next(minAtk, maxAtk + 1);
-        int totalDamage = atk - enemy.defense;
-        Debug.Log(" the attack is " + atk + "total damage =" + totalDamage + " left life = " + enemy.actualHealth);
+        cooling = false;
+        timer = stats.attackSpeed;
 
-        enemy.TakeDamage(atk);
-        animator.SetBool("Attack", true);
-        if(!enemy.alive)
+    }
+    
+}
+
+public void BackToIdle()
+{
+    animator.SetBool("Attack", false);
+}
+public void BattleLogic()
+{
+    if(stats.alive)
+    {
+        if(enemy != null)
         {
-            enemy = null;
-        }
-    }
-    public void TakeDamage(int atk)
-    {
-        actualHealth -= Mathf.Max(0, atk - stats.defense);
-        
-        if(actualHealth <= 0)
-        {
-            Death();   
-        }
-        
-    }
-    public void Death()
-    {
-        stats.alive = false;
-        coll.enabled = false;
-        animator.SetTrigger("Death");
-    }
-    public void LevelUp()
-    {
-
-    }
-    public void TakeExp(int exp)
-    {
-
-    }
-    public void Cooldown()
-    {
-        timer -= Time.deltaTime;
-
-        if (timer <= 0 && cooling)
-        {
-            cooling = false;
-            timer = stats.attackSpeed;
-
-        }
-        
-    }
-
-    public void BackToIdle()
-    {
-        animator.SetBool("Attack", false);
-    }
-    public void BattleLogic()
-    {
-        if(stats.alive)
-        {
-            if(enemy != null)
+            if(enemy.alive)
             {
-                if(enemy.alive)
+                if(!cooling)
                 {
-                    if(!cooling)
-                    {
-                        Attack(stats.minAttack, stats.maxAttack);
-                        cooling = true;
-                    }
-                    else
-                    {
-                        
-                        Cooldown();
-                    }
+                    Attack(stats.minAttack, stats.maxAttack);
+                    cooling = true;
+                }
+                else
+                {
+                    
+                    Cooldown();
                 }
             }
-            else
-            {
-                BackToIdle();
-                timer = stats.attackSpeed;
-            }  
         }
-             
-    }
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.tag == "Enemy")
+        else
         {
-            enemy = other.gameObject.GetComponent<Enemy>();
-        }
+            BackToIdle();
+            timer = stats.attackSpeed;
+        }  
     }
+         
+}
+private void OnTriggerEnter2D(Collider2D other) 
+{
+    if(other.tag == "Enemy")
+    {
+        enemy = other.gameObject.GetComponent<Enemy>();
+    }
+}
 
 }
